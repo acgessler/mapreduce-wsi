2b072f97858d74c78b7831b377ca8e400cd412bb
Alexander Gessler <alexander.gessler@gmx.net>
2014-06-24 13:44:48 +0200

    Forgot file.

---
+++ src/de/uni_stuttgart/ipvs_as/MapReduceWSIException.java
@@ -0,0 +1,14 @@
+package de.uni_stuttgart.ipvs_as;
+
+public class MapReduceWSIException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5117788169530315446L;
+
+	public MapReduceWSIException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+}
